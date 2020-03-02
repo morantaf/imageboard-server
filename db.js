ba8ec0375b0012c8c30abdae4c0a51b367641a93
morantaf <fall_moranta@live.fr>
2020-03-02 11:04:27 +0100

    Connecting database

---
+++ db.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize");
+const databaseURL =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:password@localhost:5432/postgres";
+const db = new Sequelize(databaseURL);
+
+db.sync()
+  .then(console.log("Database synchronized"))
+  .catch(err => console.log(err));
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const db = require("./db.js");
+
 const jsonParser = express.json();
 const port = process.env.PORT || 4000;
 
